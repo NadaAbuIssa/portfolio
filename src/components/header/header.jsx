e47d0b191d368a66278bbeb55887aae9d15958bb
Nada <1212383@student.birzeit.edu>
2024-12-13 22:02:49 +0200

    update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -42,14 +42,17 @@ export default function Header() {
 
   return (
     <header className="z-[999] relative">
-      <div className="fixed w-full flex flex-col justify-center items-center bg-bkg-2/60 border border-text/10 h-[7.25rem] inset-0 my-0 mx-auto shadow-lg shadow-black/[0.1] md:w-[60rem] md:h-[5.5rem] md:rounded-full md:top-5 2xl:w-[80rem] overflow-hidden">
+      <div
+        id="nav"
+        className="fixed text-base flex flex-col justify-center items-center bg-bkg-2/60 border border-text/10 h-[7.25rem] inset-0 my-0 mx-auto shadow-lg shadow-black/[0.1] md:w-[60rem] md:h-[5.5rem] md:rounded-full md:top-5 2xl:w-[80rem]  overflow-hidden"
+      >
         <nav className="h-12 flex fixed left-1/2 -translate-x-1/2 sm:h-[initial] sm:rounded-full">
           {/* logo img */}
           <div className="relative flex items-center overflow-hidden user-select-none">
             <img
               src={catlogo}
               alt="logo"
-              className="logo m-1 float-left h-12 w-12 sm:h-20 sm:w-20 transition-transform duration-[1000ms] ease-in-out"
+              className="logo m-1 float-left h-12 w-12 sm:h-20 sm:w-20 transition-transform duration-[1000ms] ease-in-out sm:display-none"
               style={{
                 position: "relative",
                 display: "flex",
@@ -58,7 +61,8 @@ export default function Header() {
               }}
             />
           </div>
-          <ul className="flex flex-wrap gap-2 justify-center text-sm items-center w-full sm:w-[40rem] md:gap-5 md:text-base">
+
+          <ul className="flex flex-wrap gap-2 justify-center text-sm text-xs items-center w-full sm:w-[40rem] md:gap-5 md:text-base">
             {linkData.map((link) => (
               <li
                 key={link.id}
